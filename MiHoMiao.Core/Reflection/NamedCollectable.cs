8f5f6cf5f0c67d57e29b39ee9b67fb0ccd6f9db2
Jasson <11642743+jasson-liao@user.noreply.gitee.com>
2025-07-27 18:51:39 +0800

    更新了核心场景资源

---
+++ MiHoMiao.Core/Reflection/NamedCollectable.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+namespace MiHoMiao.Core.Reflection;
+
+public abstract class NamedCollectable : ICollectable
+{
+    /// <summary>
+    /// 可读性更高的一个中文名称.
+    /// 收集时不会被使用. 仅用于前端显示.
+    /// </summary>
+    public abstract string ChineseName { get; }
+    
+    /// <summary>
+    /// 可读性更高的一个英文名称.
+    /// </summary>
+    public abstract string EnglishName { get; }
+    
+    /// <summary>
+    /// 该收集对象的包信息
+    /// </summary>
+    public PackageInfoAttribute? PackageInfo => field ??= GetType().Assembly.GetCustomAttribute<PackageInfoAttribute>();
+
+    public string UniqueName => $"{PackageInfo?.Name}::{EnglishName}";
+
+    [field: AllowNull, MaybeNull]
+    public string[][] NameGroups => field ??=
+    [
+        [ChineseName, PackageInfo?.Name!],
+        [EnglishName, PackageInfo?.Name!]
+    ];
+
+}
---
+++ MiHoMiao.Core/Reflection/PackageInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace MiHoMiao.Core.Reflection;
+
+[AttributeUsage(AttributeTargets.Assembly)]
+public sealed class PackageInfoAttribute(string name, Version version) : Attribute
+{
+    public string Name { get; } = name;
+    public Version Version { get; } = version;
+}
