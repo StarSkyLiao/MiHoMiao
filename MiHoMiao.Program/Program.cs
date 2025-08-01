b9a310ce006e1e90a28bef573a648b1d0eb975cf
liaomingxuan <liaomingxuan@bytedance.com>
2025-08-01 16:03:31 +0800

    一些 unsafe 代码的测试

--- MiHoMiao.Program/Program.cs
+++ MiHoMiao.Program/Program.cs
@@ -1,3 +1,14 @@
-﻿using MiHoMiao.Program.Jarfter;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
-ConsoleProgram.Run();
+// ConsoleProgram.Run();
+unsafe
+{
+    string input = "1111";
+    ReadOnlySpan<byte> readOnlySpan = MemoryMarshal.AsBytes(input.AsSpan());
+    ref byte reference = ref MemoryMarshal.GetReference(readOnlySpan);
+    void* pointer = Unsafe.AsPointer(ref Unsafe.AddByteOffset(ref reference, new IntPtr(-4)));
+    *(int*)pointer = 1;
+    Console.Write(input);
+}
+
