640836ea532a1a1e2d92007112dc14cadce0c1f9
liaomingxuan <liaomingxuan@bytedance.com>
2025-08-22 19:04:13 +0800

    暂时注释掉了 FieldToken

--- MiHoMiao.Migxn/Syntax/Lexers/Tokens/Operators/Calc/FieldToken.cs
+++ MiHoMiao.Migxn/Syntax/Lexers/Tokens/Operators/Calc/FieldToken.cs
@@ -1,17 +1,24 @@
-using MiHoMiao.Migxn.Syntax.Grammars.Expressions.Binary;
-using MiHoMiao.Migxn.Syntax.Lexers.Tokens.Keywords;
-
-namespace MiHoMiao.Migxn.Syntax.Lexers.Tokens.Operators.Calc;
-
-internal record FieldToken(int Index, (int Line, int Column) Position)
-    : MigxnOperator(UniqueName.AsMemory(), Index, Position), IOperatorToken, IClosedBinaryToken
-{
-    public static string UniqueName => ".";
-
-    public static MigxnOperator Create(int index, (int Line, int Column) position) => new FieldToken(index, position);
-
-    int IBinaryToken.Priority => 0;
-    
-    MigxnNode ILeaderOpToken.MigxnNode => this;
-
-}
+// using MiHoMiao.Migxn.Runtime;
+// using MiHoMiao.Migxn.Syntax.Grammars.Expressions;
+// using MiHoMiao.Migxn.Syntax.Grammars.Expressions.Binary;
+// using MiHoMiao.Migxn.Syntax.Lexers.Tokens.Keywords;
+//
+// namespace MiHoMiao.Migxn.Syntax.Lexers.Tokens.Operators.Calc;
+//
+// internal record FieldToken(int Index, (int Line, int Column) Position)
+//     : MigxnOperator(UniqueName.AsMemory(), Index, Position), IOperatorToken, IClosedBinaryToken
+// {
+//     public static string UniqueName => ".";
+//
+//     public static MigxnOperator Create(int index, (int Line, int Column) position) => new FieldToken(index, position);
+//
+//     int IBinaryToken.Priority => 0;
+//     
+//     public Type BinaryType(MigxnExpr left, MigxnExpr right, MigxnContext context)
+//     {
+//         throw new NotImplementedException();
+//     }
+//
+//     MigxnNode ILeaderOpToken.MigxnNode => this;
+//
+// }
