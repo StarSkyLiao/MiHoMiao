31b08af712d8ba8b3582c50d41bcdee3a0135c8d
liaomingxuan <liaomingxuan@bytedance.com>
2025-08-22 18:52:04 +0800

    移位运算符

--- MiHoMiao.Migxn/Syntax/Lexers/Tokens/Operators/Logic/ShlToken.cs
+++ MiHoMiao.Migxn/Syntax/Lexers/Tokens/Operators/Logic/ShlToken.cs
@@ -1,17 +1,21 @@
 using MiHoMiao.Migxn.Syntax.Grammars.Expressions.Binary;
 using MiHoMiao.Migxn.Syntax.Lexers.Tokens.Keywords;
+using MiHoMiao.Migxn.Syntax.Parser.Intermediate;
+using MiHoMiao.Migxn.Syntax.Parser.Intermediate.Algorithm;
 
 namespace MiHoMiao.Migxn.Syntax.Lexers.Tokens.Operators.Logic;
 
 internal record ShlToken(int Index, (int Line, int Column) Position)
-    : MigxnOperator(UniqueName.AsMemory(), Index, Position), IOperatorToken, IBinaryToken
+    : MigxnOperator(UniqueName.AsMemory(), Index, Position), IOperatorToken, IIntShiftBinary<ShlToken>
 {
     public static string UniqueName => "<<";
 
     public static MigxnOperator Create(int index, (int Line, int Column) position) => new ShlToken(index, position);
-
-    int IBinaryToken.Priority => 6;
     
+    static MigxnOpCode IIntShiftBinary<ShlToken>.Operator { get; } = new OpShl();
+    
+    int IBinaryToken.Priority => 6;
+
     MigxnNode ILeaderOpToken.MigxnNode => this;
 
 }
--- MiHoMiao.Migxn/Syntax/Lexers/Tokens/Operators/Logic/ShrToken.cs
+++ MiHoMiao.Migxn/Syntax/Lexers/Tokens/Operators/Logic/ShrToken.cs
@@ -1,15 +1,19 @@
 using MiHoMiao.Migxn.Syntax.Grammars.Expressions.Binary;
 using MiHoMiao.Migxn.Syntax.Lexers.Tokens.Keywords;
+using MiHoMiao.Migxn.Syntax.Parser.Intermediate;
+using MiHoMiao.Migxn.Syntax.Parser.Intermediate.Algorithm;
 
 namespace MiHoMiao.Migxn.Syntax.Lexers.Tokens.Operators.Logic;
 
 internal record ShrToken(int Index, (int Line, int Column) Position)
-    : MigxnOperator(UniqueName.AsMemory(), Index, Position), IOperatorToken, IBinaryToken
+    : MigxnOperator(UniqueName.AsMemory(), Index, Position), IOperatorToken, IIntShiftBinary<ShrToken>
 {
     public static string UniqueName => ">>";
 
     public static MigxnOperator Create(int index, (int Line, int Column) position) => new ShrToken(index, position);
 
+    static MigxnOpCode IIntShiftBinary<ShrToken>.Operator { get; } = new OpShr();
+    
     int IBinaryToken.Priority => 6;
     
     MigxnNode ILeaderOpToken.MigxnNode => this;
