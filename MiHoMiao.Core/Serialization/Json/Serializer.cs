b3713aa2b30258976d3c321e4b572adc9eb6c0d6
liaomingxuan <liaomingxuan@bytedance.com>
2025-10-15 13:07:25 +0800

    现在不再使用 Newtonsoft.Json 包, MiHoMiao.Core.Serialization.Json 的实现现在采用 System.Text.Json 命名空间. 不再使用 YamlDotNet.Serialization 包, 现在不再提供 Yaml 的相关方法

--- MiHoMiao.Core/Serialization/Json/Serializer.cs
+++ MiHoMiao.Core/Serialization/Json/Serializer.cs
@@ -1,4 +1,4 @@
-﻿using Newtonsoft.Json;
+﻿using System.Text.Json;
 
 namespace MiHoMiao.Core.Serialization.Json;
 
@@ -7,12 +7,10 @@ public static class Serializer
     /// <summary>
     /// 将一个 DataObject 对象转化为 Json 字符串
     /// </summary>
-    public static string ToJson<T>(this T dataObject)
-        => JsonConvert.SerializeObject(dataObject);
+    public static string ToJson<T>(this T dataObject) => JsonSerializer.Serialize(dataObject);
 
     /// <summary>
     /// 将一个 Json 字符串解析为 DataObject 对象
     /// </summary>
-    public static T? FromJson<T>(this string jsonText)
-        => JsonConvert.DeserializeObject<T>(jsonText);
+    public static T? FromJson<T>(this string jsonText) => JsonSerializer.Deserialize<T>(jsonText);
 }
